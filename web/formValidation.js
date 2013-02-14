e2ac01a8cf5a693ce82406423844aa96aa7428bf
Jonathon <Jonathon@JonathonsLaptop.att.net>
2013-02-14 13:12:23 -0600

    Fully Done

--- web/formValidation.js
+++ web/formValidation.js
@@ -4,25 +4,25 @@ function checkForms(type){
         if(type == "rectangle"){
             if (regexChecker.test(rectangleAreaCalculator.txtWidth.value)==false){
                 rectangleAreaCalculator.txtWidth.focus();
-                throw new Error("You need a numeric value in this field.");
+                throw new Error("You need a positive, numeric value in this field.");
             }else if (regexChecker.test(rectangleAreaCalculator.txtHeight.value)==false){
                 rectangleAreaCalculator.txtHeight.focus();
-                throw new Error("You need a numeric value in this field.");
+                throw new Error("You need a positive, numeric value in this field.");
             }
             return(true);
         }else if (type == "circle"){
             if (regexChecker.test(circleAreaCalculator.txtRadius.value)==false){
                 circleAreaCalculator.txtRadius.focus();
-                throw new Error("You need a numeric value in this field.");
+                throw new Error("You need a positive, numeric value in this field.");
             }
             return(true);
         }else if (type == "triangleSide"){
             if (regexChecker.test(triangleSideCalculator.txtSideA.value)==false){
                 triangleSideCalculator.txtSideA.focus();
-                throw new Error("You need a numeric value in this field.");
+                throw new Error("You need a positive, numeric value in this field.");
             }else if (regexChecker.test(triangleSideCalculator.txtSideB.value)==false){
                 triangleSideCalculator.txtSideB.focus();
-                throw new Error("You need a numeric value in this field.");
+                throw new Error("You need a positive, numeric value in this field.");
             }
             return(true);
         }
